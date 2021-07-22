a496769f7ef7ed6ca7fe0f960cc0d2d357ed23b2
yqq <1037123792@qq.com>
2021-07-22 17:40:14 +0800

    update package

--- src/com/yqq/thread/ch02/p88/MyService.java
+++ src/com/yqq/thread/ch02/p88/MyService.java
@@ -4,11 +4,9 @@ public class MyService {
 
     public MyOneList addServiceMethod(MyOneList list, String data){
         try{
-            synchronized (list){
-                if (list.getSize() < 1) {
-                    Thread.sleep(2000); //模拟从远程花费2秒取回数据
-                    list.add(data);
-                }
+            if (list.getSize() < 1) {
+                Thread.sleep(2000); //模拟从远程花费2秒取回数据
+                list.add(data);
             }
         }catch (InterruptedException e){
             e.printStackTrace();
--- src/com/yqq/thread/ch02/p90/MyService.java
+++ src/com/yqq/thread/ch02/p90/MyService.java
@@ -4,9 +4,11 @@ public class MyService {
 
     public MyOneList addServiceMethod(MyOneList list, String data){
         try{
-            if (list.getSize() < 1) {
-                Thread.sleep(2000); //模拟从远程花费2秒取回数据
-                list.add(data);
+            synchronized (list){
+                if (list.getSize() < 1) {
+                    Thread.sleep(2000); //模拟从远程花费2秒取回数据
+                    list.add(data);
+                }
             }
         }catch (InterruptedException e){
             e.printStackTrace();
