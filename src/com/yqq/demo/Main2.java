91c2eca0449ad09957a4ca9b692e9202a5b0e092
yqq <1037123792@qq.com>
2021-09-14 13:36:54 +0800

    code update

---
+++ src/com/yqq/demo/Main2.java
@@ -0,0 +1,19 @@
+package com.yqq.demo;
+
+/**
+ * description:
+ * Created by yqq
+ * 2021-09-07
+ */
+public class Main2 {
+    public static void main(String[] args) {
+
+        Integer a = null;
+        Integer b = 7 ;
+
+        if((a != null && a > 0) || b > 0){
+            System.out.println("hello");
+        }
+
+    }
+}
